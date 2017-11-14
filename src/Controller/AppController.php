9ec958e70a12556be52afe626d9a3ffdebef893f
Cessare Julius <djcsarjulio1997@gmail.com>
2017-11-14 16:48:16 -0400

    estetica

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -103,8 +103,7 @@ class AppController extends Controller
 
     public function isAuthorized($user)
     {
-        if(isset($user['role']) and $user['role'] === 'admin')
-        {
+        if (isset($user['role']) and $user['role'] === 'admin') {
             return true;
         }
 
